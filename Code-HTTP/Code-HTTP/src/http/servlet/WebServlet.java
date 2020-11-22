dcc54a8dbbc63971cb47ee5f0dbc2e707f36eeab
HZjiadong <guojiadonghz@gmail.com>
2020-11-22 20:18:29 +0800

    Create WebServlet.java

---
+++ Code-HTTP/Code-HTTP/src/http/servlet/WebServlet.java
@@ -0,0 +1,30 @@
+package http.servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class WebServlet extends HttpServlet {
+
+    /**
+     * A simple HTTP Servlet handling a form*/
+    public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String name = req.getParameter("name");
+
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE> What For </TITLE></HEAD>");
+        out.println("<BODY><BIG> What For </BODY></BIG>");
+        out.println("</HTML>");
+        out.close();
+
+        String someId = req.getParameter("someId");
+        if (someId != null){
+            out.println(someId);
+        }
+    }
+}
