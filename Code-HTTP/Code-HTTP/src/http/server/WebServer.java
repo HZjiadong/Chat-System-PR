e787fa8d72948d2f856c0955cadb7bbce2e71e38
HZjiadong <guojiadonghz@gmail.com>
2020-11-22 20:18:08 +0800

    Update WebServer.java

--- Code-HTTP/Code-HTTP/src/http/server/WebServer.java
+++ Code-HTTP/Code-HTTP/src/http/server/WebServer.java
@@ -264,27 +264,7 @@ public class WebServer {
 
   }
 
-
-  /**
-  * A simple HTTP Servlet handling a form*/
-  public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException {
-
-    res.setContentType("text/html");
-    PrintWriter out = res.getWriter();
-
-    String name = req.getParameter("name");
-    
-    out.println("<HTML>");
-    out.println("<HEAD><TITLE> What For </TITLE></HEAD>");
-    out.println("<BODY><BIG> What For </BODY></BIG>");
-    out.println("</HTML>");
-    out.close();
-
-    String someId = req.getParameter("someId");
-    if (someId != null){
-      out.println(someId);
-    }
-  }
+  
 
   public String getContentType(File file) {
 
